7b42c43346ab306dcd7e2b09904eb974f32160e7
VLuca997 <kulertoast997@gmail.com>
2023-06-15 18:26:33 +0200

    Modifiche al JS, inizializzazione di: mostrare il risultato e roll number

--- DADI/js/script.js
+++ DADI/js/script.js
@@ -32,3 +32,43 @@
 
 // Event listener per il pulsante "Reset"
 
+//generiamo un numero casuale:
+function getRandomNumber(min, max) {
+
+    return Math.floor(Math.random() * (min, max + 1)) + min;  // la funzione scritta restituisce un numero casuale tra il minimo e il massimo. (1 / 6 da aggiungere in seguito)
+}
+
+//funzione per vedere il risultato
+function showResult(result) { // result è il paramentro che rappresenterà il risultato.
+
+    let resultDiv = document.getElementById('result'); //Seleziona l'elemento HTML con l'id result e lo assegna alal variabile resultDiv
+
+    resultDiv.innerHTML = result; // impostiamo il contenuto html con il valore del parametro result
+
+    if(result === "Hai Vinto, Stupidissimo Umano!"){
+        resultDiv.style.backgroundColor ="green"; // se vince impsota il colore della barra dei risultati a Verde
+    }
+    else if(result === "Ha Vinto il Computer, Looooooser!"){ 
+        resultDiv.style.backgroundColor = "green"; // se vince impsota il colore della barra dei risultati a Verde
+    }
+    else {
+        resultDiv.style.backgroundColor = "yellow"; // nel caso di pareggio, impostiamo lo sfondo della barra dei risultati a Giallo
+    }
+}
+
+
+function rollDice() {
+    let playScore = getRandomNumber (1, 6); // Genera un punteggio casuale per il giocatore
+    let computerScore = getRandomNumber (1, 6);// Genera un punteggio casuale per il computer
+
+    let playerDiv = document.getElementById('player');// selezioniamo l'elemento HTML del player
+    let computerDiv = document.getElementById('computer'); // selezioniamo l'elemento html del computer
+
+    playerDiv.innerHTML = playScore;    // impostiamo il punteggio del player come contenuto nell'elemento dell'html
+    computerDiv.innerHTML = computerScore; // impostiamo il punteggio del computer come contenuto nell'elemento dell'html
+
+    playerDiv.style.backgroundColor = "red"; // impostiamo lo sfondo dell'elemento html a rosso ( servirà per la sconfitta )
+    computerDiv.style.backgroundColor = "red"; // impostiamo lo sfondo dell'elemento html a rosso ( servirà per la sconfitta )
+    
+
+}
