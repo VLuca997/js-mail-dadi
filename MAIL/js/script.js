d8ec2ce7707941ecf40ae1c8e5e60db756e55d97
VLuca997 <kulertoast997@gmail.com>
2023-06-16 00:55:33 +0200

    inizio esercizio con html/form e logica JS

---
+++ MAIL/js/script.js
@@ -0,0 +1,4 @@
+// 1. Come poss oottenere l'input dell'utente dalla email e password?
+// 2.Come posso vedere e capire se l'email che viene inserita è corretta per accedere?
+// 3. quali azioni posso fare se l'email è nella lista di accesso? e se non è nella lista?
+// 4. mostrare messaggio di non avvenuto o avvenuto accesso alla pagina.
