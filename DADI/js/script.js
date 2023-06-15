548ce0bdc696e82f7d27f51faa61cdeb41cd3786
VLuca997 <kulertoast997@gmail.com>
2023-06-15 17:38:46 +0200

    inizializzazione gioco DADI, HTML e CSS.

---
+++ DADI/js/script.js
@@ -0,0 +1,11 @@
+//-------------------------------------------------------------------------//
+// Funzione per generare un numero casuale tra 1 e 6 (inclusi)
+//
+// Funzione per generare il lancio dei dadi
+
+// Funzione per resettare il gioco
+
+// Event listener per il pulsante "Lancia i dadi"
+
+// Event listener per il pulsante "Reset"
+
