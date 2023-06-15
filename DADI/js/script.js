54ddab9a3be2f7b7f141b0af4a7ffe8159d00551
VLuca997 <kulertoast997@gmail.com>
2023-06-15 18:01:00 +0200

    testo JS per risoluzione problem solving

--- DADI/js/script.js
+++ DADI/js/script.js
@@ -1,9 +1,32 @@
-//-------------------------------------------------------------------------//
-// Funzione per generare un numero casuale tra 1 e 6 (inclusi)
-//
-// Funzione per generare il lancio dei dadi
+//-------------------------------DADI--------------------------------------//
+/* 1: Funzione per generare un numero casuale tra 1 e 6 (inclusi)
+    -generare i lanci dei dadi per pg1 e computer
+*/
+/* funzione per visualizzare il risultato
+    - selezioniamo l'elemento html che deve mostrare il risultato
+    - immettiamo il testo all'interno dell'elemento html con il risultato
+    - verificare il risultato per determinare il vincitore e imposta il colore di sfondo corrispondente ( win in green )
+    - con un if, else if, else, decidiamo che colore di sfondo dare ai vincitori, nell'else finale diamo il colore per il pareggio.
+    - 
+*/
+/* Funzione per generare il lancio dei dadi
+    -generiamo dei punteggi randomici per player 1 e computer
+    -selezioniamo gli elementi HTML del player 1 e computer
+    -mostriamo i risultati randomici nei 2 div creati nell'html
+    -diamo colore di sfondo agli elementi html ( lose in red )
+    -Determina il vincitore in base ai punteggi dei giocatori e imposta il colore di sfondo corrispondenteù
+    -Se il giocatore ha il punteggio più alto, imposta il colore di sfondo del suo elemento su verde
+    - Mostriamo il messaggio di vittoria del giocatore utilizzando la funzione showResult
+    -Se il computer ha il punteggio più alto, imposta il colore di sfondo del suo elemento su verde
+    -Mostra il messaggio di vittoria del computer utilizzando la funzione showResult
+    -Se i punteggi sono uguali, imposta il colore di sfondo di entrambi gli elementi su giallo
+    -Mostra il messaggio di pareggio utilizzando la funzione showResult
 
-// Funzione per resettare il gioco
+*/
+
+/* Funzione per resettare il gioco
+    -
+*/
 
 // Event listener per il pulsante "Lancia i dadi"
 
